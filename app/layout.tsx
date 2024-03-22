68180db630fabe3e031f256f17cba8a2b4358758
Talal Ahmed <talalsidd31@gmail.com>
2024-03-22 20:19:43 +0500

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 export const metadata: Metadata = {
   title: "Travel",
   description: "Travel UI/UX App for Camping",
+  
 };
 
 export default function RootLayout({
