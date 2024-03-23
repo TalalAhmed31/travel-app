f7ae699286b40733d9e3c311d8ae2f7d04d795c8
Talal Ahmed <talalsidd31@gmail.com>
2024-03-23 17:44:45 +0500

    added constants to map over

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,22 @@
-import Link from "next/link"
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="border-2 border-red-500 flexBetween max-cxontainer padding-container realtive z-30 py-5">
       <Link href="/">
-        home
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key}>
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-  }
+  );
+};
 
-export default Navbar
+export default Navbar;
