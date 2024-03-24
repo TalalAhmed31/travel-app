d65835163179bf26760860181378025b7f9cf931
Talal Ahmed <talalsidd31@gmail.com>
2024-03-24 17:53:15 +0500

    navbar mobile responsiveness done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
@@ -26,6 +26,14 @@ const Navbar = () => {
         variant= "btn_dark_green"
         />
       </div>
+
+      <Image 
+      src="menu.svg"
+      alt="menu"
+      width={32}
+      height={32}
+      className=" inline-block cursor-pointer lg:hidden"
+       />
     </nav>
   );
 };
